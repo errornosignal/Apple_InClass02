359bdd4ac7aab628fff166133d26db2c8fd5bb69
errornosignal <errornosignal@users.noreply.github.com>
2017-09-07 10:21:26 -0500

    init commit

--- Apple_InClass02/main.swift
+++ Apple_InClass02/main.swift
@@ -8,5 +8,15 @@
 
 import Foundation
 
-print("Hello, World!")
+var numbers: [Int] = []
+var constants = [3.14, 6.28, 100]
 
+for i in 0...27 {
+    numbers.append(i*i)
+}
+//print("numbers countains \(numbers)")
+//print("contants contains \(constants.count) elements = \(constants)")
+
+for i in stride(from:numbers.count-1, through:0, by:-3) {
+    print("numbers[\(i)] = \(numbers[i])")
+}
