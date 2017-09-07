fb2a89bbe3fb0bb18693d1cc465ef2f8092ed244
errornosignal <errornosignal@users.noreply.github.com>
2017-09-07 10:06:27 -0500

    Initial Commit

---
+++ Apple_InClass02/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Apple_InClass02
+//
+//  Created by Reid Nolan on 9/7/17.
+//  Copyright © 2017 Reid Nolan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
